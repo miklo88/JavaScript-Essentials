ddfcd1560a4524f1ad11a1416cfb1500cd6ccbb7
Carl Redding <credding07@gmail.com>
2020-05-16 15:37:44 -0500

    created for-loopin file. for some loopins

--- Comparison-Conditional-Execution/Condition.js
+++ Comparison-Conditional-Execution/Condition.js
@@ -1,11 +1,13 @@
 //28// Conditions - defining the instructions of what you want to run.
 const renderToDOM = 28,
   render = "yes";
-
+//if renderToDOM is not equal to 28(anything besides 28) render ok
 if (renderToDOM != 28) {
   console.log("ok");
+  // else if renderToDOM is 28 AND render is "yes" run the console.log
 } else if (renderToDOM === 28 && render === "yes") {
   console.log("//28// Conditions!");
+  //if all of this doesnt match then we have a problemo.
 } else {
   console.log("uh oh");
 }
