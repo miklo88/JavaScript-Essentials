6ddfa177c1571150f3700ada54807f5f2d0d813b
Carl Redding <credding07@gmail.com>
2020-05-16 15:24:14 -0500

    just have to come up with a ton of different conditions to get the idea and better flow of how they work and what i can do with them.

--- Comparison-Conditional-Execution/Condition.js
+++ Comparison-Conditional-Execution/Condition.js
@@ -1,4 +1,14 @@
 //28// Conditions - defining the instructions of what you want to run.
+const renderToDOM = 28,
+  render = "yes";
+
+if (renderToDOM != 28) {
+  console.log("ok");
+} else if (renderToDOM === 28 && render === "yes") {
+  console.log("//28// Conditions!");
+} else {
+  console.log("uh oh");
+}
 
 const carsLeft = 1,
   carsRight = 0,
