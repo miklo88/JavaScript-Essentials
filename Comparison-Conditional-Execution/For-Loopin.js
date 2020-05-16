8a5864ca3f2e371e645eb6686ab1aee86edcfb0d
Carl Redding <credding07@gmail.com>
2020-05-16 16:55:28 -0500

    for loop examples created.

--- Comparison-Conditional-Execution/For-Loopin.js
+++ Comparison-Conditional-Execution/For-Loopin.js
@@ -1,1 +1,21 @@
 //29// For-Loops
+
+//basically looping through an array of data and being able to select specific items. You can add, delete data as well.
+
+// if(){} - is a singular conditon. Good for executing just once.
+//for(){} -is a plural, running your context multiple times.
+
+const classRegister = ["John", "Erykah", "Samuel", "Carlos", "Amelia", "Celia"];
+
+//index counter - var i = 0;
+// condition if i is less than classRegister - 0 < classRegister.length;
+//incrementing value - i++
+for (var i = 0; i < classRegister.length; i++) {
+  console.log(classRegister[i]);
+}
+
+var text = "";
+var i;
+for (var i = 0; i < 20; i++) {
+  console.log((text = "Number " + [i]));
+}
