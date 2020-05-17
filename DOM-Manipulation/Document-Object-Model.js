86871b329c83eaaa471f771beb839ab0e3fd5eef
Carl Redding <credding07@gmail.com>
2020-05-17 10:31:25 -0500

    first commit, index html and DOM.js file are created.

---
+++ DOM-Manipulation/Document-Object-Model.js
@@ -0,0 +1,10 @@
+const DOM = "Document Object Model";
+const subDOM = 10;
+
+if (subDOM != 10) {
+  console.log("Hey now");
+} else if (DOM === "Document Object Model" && subDOM === 10) {
+  console.log(DOM);
+} else {
+  console.log("uh oh, we have a bigger problem on our hands");
+}
