87bf73830fb09dc411c987548ceff76d3113b0c0
Carl Redding <credding07@gmail.com>
2020-05-17 21:41:05 -0500

    having fun with functionality in javascript. it honestly feels good to be able to write something and have it execute. carlitos has come a long way.

--- DOM-Manipulation/Event-Handler.js
+++ DOM-Manipulation/Event-Handler.js
@@ -1,3 +1,11 @@
+function CreateElement() {
+  const bob = "//35// Event Handlers";
+  const h1 = document.createElement("h1");
+  h1.innerHTML = bob;
+  document.body.appendChild(h1);
+  return bob;
+}
+console.log(CreateElement());
 //35// Event Handlers
 //Events are simply when something happens in our application and something happens
 // on mouse click, button click etc. it triggers functions subroutines etc
@@ -25,3 +33,19 @@ select.addEventListener("click", ClickCallback); //adds it
 //to remove an eventListener
 select.removeEventListener("click", ClickCallback);
 //by the time you look in the console its already too late! its gone!
+
+////////////36 Appendchild and create elements
+const divyDiv = document.createElement("div");
+divyDiv.innerHTML = "//36// Create and appendChild and insertBefore methods.";
+divyDiv.onclick = function () {
+  alert("secret click");
+};
+document.body.appendChild(divyDiv);
+
+const element = document.createElement("div");
+element.innerHTML = "Click me, i'm a needy button";
+element.style.cssText = "width:200px; height:50px; background:red;";
+element.onclick = function () {
+  alert("Hi");
+};
+document.body.appendChild(element);
