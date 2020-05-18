2bc434ae4bc2387720a810d403c443e71ddb6a14
Carl Redding <credding07@gmail.com>
2020-05-17 22:25:02 -0500

    dried up the code DRY 2020 and really really glad i did this course because my lightbulb of confidence is beamin like crazy meow. Looking forward to following Lawrence on social media and see what else he teaches. Between him and Brad Traversy you can't help to fall in love with JavaScript

--- Final/Final-Project.js
+++ Final/Final-Project.js
@@ -1,12 +1,29 @@
+//code before cleanup DRY 2020
+
+// function set() {
+//   let elements = document.getElementsByName("cssProperty");
+//   //   console.log(elements);
+//   for (let index = 0; index < elements.length; index++) {
+//     let cssProperty = elements[index].getAttribute("id");
+//     let cssValue = elements[index].value;
+//     console.log(cssProperty, cssValue);
+
+//     let div = document.getElementById("modify");
+//     div.style[cssProperty] = cssValue;
+//   }
+// }
+// document.getElementById("set").addEventListener("click", set);
+
+//After DRY 2020
+let elements = document.getElementsByName("cssProperty");
+
+let div = document.getElementById("modify");
+
 function set() {
-  let elements = document.getElementsByName("cssProperty");
-  //   console.log(elements);
   for (let index = 0; index < elements.length; index++) {
     let cssProperty = elements[index].getAttribute("id");
-    let cssValue = elements[index].value;
-    console.log(cssProperty, cssValue);
 
-    let div = document.getElementById("modify");
+    let cssValue = elements[index].value;
     div.style[cssProperty] = cssValue;
   }
 }
